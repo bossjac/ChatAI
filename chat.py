ca938904d5501025d06c18639053d76774c3b80e
bossjac <69126568+bossjac@users.noreply.github.com>
2024-05-10 22:06:07 +0100

    Update chat.py

--- chat.py
+++ chat.py
@@ -20,6 +20,10 @@ if st.button("Send"):
             audio_url = "https://kingnish-jarvis.hf.space/file=" + result
             # Display the audio with autoplay enabled
             audio_response = requests.get(audio_url)
-            st.audio(BytesIO(audio_response.content), format='audio/wav', autoplay=True)
+            
+            # Process the audio to remove "slash s" phrase
+            audio_content = audio_response.content.replace(b"slash s", b"")
+            
+            st.audio(BytesIO(audio_content), format='audio/wav', autoplay=True)
     else:
         st.warning("Please enter something.")
